881436b795e5fbfa470b027d968d81ae27948193
Junyangz <junyangz.iie@gmail.com>
2020-01-09 10:36:02 +0800

    Set member name to No.

--- js/member.js
+++ js/member.js
@@ -1,46 +1,46 @@
 var member = [
   {
     "phone": "No.1",
-    "name": "name1"
+    "name": "No.1"
   },
   {
     "phone": "No.2",
-    "name": "name2"
+    "name": "No.2"
   },
   {
     "phone": "No.3",
-    "name": "name3"
+    "name": "No.3"
   },
   {
     "phone": "No.4",
-    "name": "name4"
+    "name": "No.4"
   },
   {
     "phone": "No.5",
-    "name": "name5"
+    "name": "No.5"
   },
   {
     "phone": "No.6",
-    "name": "name6"
+    "name": "No.6"
   },
   {
     "phone": "No.7",
-    "name": "name7"
+    "name": "No.7"
   },
   {
     "phone": "No.8",
-    "name": "name8"
+    "name": "No.8"
   },
   {
     "phone": "No.9",
-    "name": "name9"
+    "name": "No.9"
   },
   {
     "phone": "No.10",
-    "name": "name10"
+    "name": "No.10"
   },
   {
     "phone": "No.11",
-    "name": "name11"
+    "name": "No.11"
   }
 ]
