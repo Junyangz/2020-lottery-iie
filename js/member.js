fa580c45c24fbae9c84dfe630f194d564951ae93
Junyangz <junyangz.iie@gmail.com>
2020-01-09 10:54:18 +0800

    Adjust memeber length and getkey function

--- js/member.js
+++ js/member.js
@@ -42,5 +42,209 @@ var member = [
   {
     "phone": "No.11",
     "name": "No.11"
+  },
+  {
+    "phone": "No",
+    "name": "No.12"
+  },
+  {
+    "phone": "No",
+    "name": "No.13"
+  },
+  {
+    "phone": "No",
+    "name": "No.14"
+  },
+  {
+    "phone": "No",
+    "name": "No.15"
+  },
+  {
+    "phone": "No",
+    "name": "No.16"
+  },
+  {
+    "phone": "No",
+    "name": "No.17"
+  },
+  {
+    "phone": "No",
+    "name": "No.18"
+  },
+  {
+    "phone": "No",
+    "name": "No.19"
+  },
+  {
+    "phone": "No",
+    "name": "No.20"
+  },
+  {
+    "phone": "No",
+    "name": "No.21"
+  },
+  {
+    "phone": "No",
+    "name": "No.22"
+  },
+  {
+    "phone": "No",
+    "name": "No.23"
+  },
+  {
+    "phone": "No",
+    "name": "No.24"
+  },
+  {
+    "phone": "No",
+    "name": "No.25"
+  },
+  {
+    "phone": "No",
+    "name": "No.26"
+  },
+  {
+    "phone": "No",
+    "name": "No.27"
+  },
+  {
+    "phone": "No",
+    "name": "No.28"
+  },
+  {
+    "phone": "No",
+    "name": "No.29"
+  },
+  {
+    "phone": "No",
+    "name": "No.30"
+  },
+  {
+    "phone": "No",
+    "name": "No.31"
+  },
+  {
+    "phone": "No",
+    "name": "No.32"
+  },
+  {
+    "phone": "No",
+    "name": "No.33"
+  },
+  {
+    "phone": "No",
+    "name": "No.34"
+  },
+  {
+    "phone": "No",
+    "name": "No.35"
+  },
+  {
+    "phone": "No",
+    "name": "No.36"
+  },
+  {
+    "phone": "No",
+    "name": "No.37"
+  },
+  {
+    "phone": "No",
+    "name": "No.38"
+  },
+  {
+    "phone": "No",
+    "name": "No.39"
+  },
+  {
+    "phone": "No",
+    "name": "No.40"
+  },
+  {
+    "phone": "No",
+    "name": "No.41"
+  },
+  {
+    "phone": "No",
+    "name": "No.42"
+  },
+  {
+    "phone": "No",
+    "name": "No.43"
+  },
+  {
+    "phone": "No",
+    "name": "No.44"
+  },
+  {
+    "phone": "No",
+    "name": "No.45"
+  },
+  {
+    "phone": "No",
+    "name": "No.46"
+  },
+  {
+    "phone": "No",
+    "name": "No.47"
+  },
+  {
+    "phone": "No",
+    "name": "No.48"
+  },
+  {
+    "phone": "No",
+    "name": "No.49"
+  },
+  {
+    "phone": "No",
+    "name": "No.50"
+  },
+  {
+    "phone": "No",
+    "name": "No.51"
+  },
+  {
+    "phone": "No",
+    "name": "No.52"
+  },
+  {
+    "phone": "No",
+    "name": "No.53"
+  },
+  {
+    "phone": "No",
+    "name": "No.54"
+  },
+  {
+    "phone": "No",
+    "name": "No.55"
+  },
+  {
+    "phone": "No",
+    "name": "No.56"
+  },
+  {
+    "phone": "No",
+    "name": "No.57"
+  },
+  {
+    "phone": "No",
+    "name": "No.58"
+  },
+  {
+    "phone": "No",
+    "name": "No.59"
+  },
+  {
+    "phone": "No",
+    "name": "No.60"
+  },
+  {
+    "phone": "No",
+    "name": "No.61"
+  },
+  {
+    "phone": "No",
+    "name": "No.62"
   }
 ]
